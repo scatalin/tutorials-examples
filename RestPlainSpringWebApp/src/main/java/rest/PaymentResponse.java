6d7cebb2daf09c6524592cc6be18ae0643c2e6ae
Catalin Stan <stan.catalin.gabriel@gmail.com>
2017-12-17 14:01:55 +0200

    [RPSWA] add request/response POJO

---
+++ RestPlainSpringWebApp/src/main/java/rest/PaymentResponse.java
@@ -0,0 +1,27 @@
+package rest;
+
+/**
+ * Base respone for the payment action
+ */
+public class PaymentResponse {
+
+    private String status;
+    private Integer code;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+}
