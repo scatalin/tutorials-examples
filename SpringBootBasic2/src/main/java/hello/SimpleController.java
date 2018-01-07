4066c1fce483dfafe6f46690a2df2c09f46f0ebe
Catalin Stan <stan.catalin.gabriel@gmail.com>
2018-01-07 19:41:16 +0200

    [SBB2] Add thymeleaf to project and made jar executable

---
+++ SpringBootBasic2/src/main/java/hello/SimpleController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author cstan
+ * @since 07.01.2018
+ */
+@Controller
+public class SimpleController {
+
+  @Value("${spring.application.name}")
+  String appName;
+
+  @GetMapping("/")
+  public String homePage(Model model) {
+    model.addAttribute("appName", appName);
+    return "home";
+  }
+}
