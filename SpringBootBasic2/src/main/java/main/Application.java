3ee1d03bb89c85b5b18abe9881283f5a9c8b120e
Catalin Stan <stan.catalin.gabriel@gmail.com>
2018-01-07 20:31:26 +0200

    [SBB2] Add simple error handling

--- SpringBootBasic2/src/main/java/main/Application.java
+++ SpringBootBasic2/src/main/java/main/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package main;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
---
+++ SpringBootBasic2/src/main/java/main/RestExceptionHandler.java
@@ -0,0 +1,34 @@
+package main;
+
+import controller.exceptions.BookIdMismatchException;
+import controller.exceptions.BookNotFoundException;
+import javax.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * @author cstan
+ * @since 07.01.2018
+ */
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler({BookNotFoundException.class})
+  protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
+    return handleExceptionInternal(ex, "Book not found", new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+  @ExceptionHandler({BookIdMismatchException.class, ConstraintViolationException.class,
+      DataIntegrityViolationException.class})
+  protected ResponseEntity<Object> handleBadRequest(Exception ex, WebRequest request) {
+    return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+  }
+
+
+}
